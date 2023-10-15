2dce27a2fdc19292c93c299cfa04e7d9db1e1110
José Lucas <joselucas065842@gmail.com>
2023-10-15 19:07:01 -0300

    New commit

--- src/main/java/br/com/joselucas/todolist/task/TaskModel.java
+++ src/main/java/br/com/joselucas/todolist/task/TaskModel.java
@@ -13,26 +13,28 @@ import lombok.Data;
 
 @Data
 @Entity(name = "tb_tasks")
-
 public class TaskModel {
-    
 
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
     private String description;
+
     @Column(length = 50)
     private String title;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String priority;
-    
+
     private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-    
-    
 
-
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
 }
